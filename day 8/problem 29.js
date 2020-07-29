b8e076bdec915331980face169c322038a301886
Hindhavi Alluru <67951932+Hindhavi@users.noreply.github.com>
2020-07-30 01:53:41 +0530

    Add files via upload

---
+++ day 8/problem 29.js
@@ -0,0 +1,24 @@
+//Print the first 100 prime numbers
+printPrimes(100);
+// Function prints the first nPrimes numbers
+function printPrimes(nPrimes)
+{
+ var n = 0;
+ var i = 2;
+ 
+ while(n < nPrimes)
+ {
+ if (isPrime(i))
+ {
+ console.log(n, “ → “, i);
+ n++;
+ }
+ 
+ i++;
+ }
+}
+// Returns true if a number is prime
+function isPrime(n)
+{
+ // your code here
+}
---
+++ day 8/problem 30.js
@@ -0,0 +1,12 @@
+//Create a function that will return in an array the first “nPrimes” prime numbers greater than a particular number “startAt”
+console.log(getPrimes(10, 100));
+function getPrimes(nPrimes, startAt)
+{
+// your code here
+ isPrime(i)
+}
+// Returns true if a number is prime
+function isPrime(n)
+{
+ // your code here
+}
